6cfddeb362e5b54ba4a8293319fd147115a98f99
“Katalyst“ <sams@katalyst.tech>
2022-01-26 11:33:53 +0000

    updated images

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="regular-text text-center">
+      <h1 class="text-3xl py-2">You seem to have lost you way</h1>
+      <router-link class="underline" :to="{ name: 'Home' }"
+        >Go Home</router-link
+      >
+    </div>
+  </div>
+</template>
